38ab719a22de2dfbf1315227fc0951e721ea325b
hellohijkl <2836428274@qq.com>
2026-08-10 13:10:27 +0800

    更新内容

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Demo site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://helloblog.de5.net",
 
 	// 站点描述
 	description:
@@ -102,13 +102,13 @@ export const siteConfig: SiteConfig = {
 		// 打赏页面开关
 		sponsor: true,
 		// 留言板页面开关，需要配置评论系统
-		guestbook: true,
+		guestbook: false,
 		// 番组计划页面开关，含追番、游戏、书籍和音乐
-		bangumi: true,
+		bangumi: false,
 		// 相册页面开关
-		gallery: true,
+		gallery: false,
 		// 追番页面开关
-		anime: true,
+		anime: false,
 		// 动态页面开关
 		dynamic: true,
 	},
